77f4eee7ac0ae65e9d19829f9a606ca7334c6b3a
Abhiram Viswam <abhiramviswam1998@gmail.com>
2022-08-02 10:20:32 +0530

    Navbar added, Homepage initialized, Home route added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <div className="workouts">
+                <h1>Home</h1>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home;
