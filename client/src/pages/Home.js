8a8db0e48ff4047288f2755e87933f7eb876a88b
Abhiram Viswam <abhiramviswam1998@gmail.com>
2022-08-03 22:05:19 +0530

    workoutForm added - able to add new workouts now

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import WorkoutDetails from "../components/WorkoutDetails";
-
+import WorkoutForm from "../components/WorkoutForm";
 const Home = () => {
     const [workouts, setWorkouts] = useState([])
     useEffect(()=>{
@@ -20,6 +20,7 @@ const Home = () => {
                     <WorkoutDetails key={workout._id} workout={workout}/>
                 )}
             </div>
+            <WorkoutForm/>
         </div>
     );
 }
