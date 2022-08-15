783391ae85db7c7d446dd3802e3c883f608542cd
Abhiram Viswam <abhiramviswam1998@gmail.com>
2022-08-15 22:46:48 +0530

    login, signup forms added
    login, signup functionality not yet implemented

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
 const Signup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return ( 
-        <h3>signup</h3>
+        <form className="signup" onSubmit={handleSubmit}>
+            <label>Email:</label>
+            <input 
+                type="email" 
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+
+            <label>Password:</label>
+            <input
+                type="password" 
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+            />
+
+            <button>Sign up</button>
+        </form>
     );
 }
  
