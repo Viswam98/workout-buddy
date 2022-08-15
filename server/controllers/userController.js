fa8321d210c094f801e41101b24ebfbab5eb9802
Abhiram Viswam <abhiramviswam1998@gmail.com>
2022-08-15 20:41:51 +0530

    jwt token being generated at server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 const loginUser = async(req,res)=>res.status(200).json({msg: 'login!'})
 
@@ -6,10 +11,9 @@ const signupUser = async(req,res) => {
     const { email, password } = req.body
     try{
         const user = await User.signup(email,password)
-        console.log('worked')
-        res.status(200).json({email,user})
+        const token = createToken(user._id)
+        res.status(200).json({email,token})
     } catch(error) {
-        console.log('error')
         res.status(400).json({error: error.message})
     }
 }
